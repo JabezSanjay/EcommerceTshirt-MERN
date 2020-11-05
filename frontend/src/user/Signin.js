4f97598854de67a877c4b91dcea3794d21adad0d
Jabez Sanjay <jbzsanjay2000@gmail.com>
2020-11-05 19:36:43 +0530

    Completed Designing Signin and Signup forms

--- frontend/src/user/Signin.js
+++ frontend/src/user/Signin.js
@@ -1,12 +1,39 @@
 import React from "react";
 import Menu from "../core/Menu";
-import "./scss/Signin.modules.scss";
+import { Link } from "react-router-dom";
+import SigninImage from "../images/signin.svg";
+import "./scss/Sign.modules.scss";
 
 const Signin = () => {
   return (
     <div>
       <Menu />
-      <div className="signin"></div>
+      <div className="align">
+        <div className="grid align__item">
+          <div className="register">
+            <img src={SigninImage} alt="" />
+            <h2>Sign In</h2>
+
+            <form action="" method="post" className="form">
+              <div className="form__field">
+                <input type="email" placeholder="Email" />
+              </div>
+
+              <div className="form__field">
+                <input type="password" placeholder="Password" />
+              </div>
+
+              <div className="form__field">
+                <input type="submit" value="Sign Up" />
+              </div>
+            </form>
+
+            <p>
+              Don't have an accout? <Link to="/signup">Sign up</Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/user/Signup.js
+++ frontend/src/user/Signup.js
@@ -1,12 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import Menu from "../core/Menu";
-import "./scss/Signup.modules.scss";
+import SignupImage from "../images/signup.svg";
+import "./scss/Sign.modules.scss";
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+
+  //Destructuring
+  const { name, email, password, error, success } = values;
+
+  //Higher order Function
+
+  const signupForm = () => {
+    return (
+      <div>
+        <div className="align">
+          <div className="grid align__item">
+            <div className="register">
+              <img src={SignupImage} alt="" />
+              <h2>Sign Up</h2>
+
+              <form action="" method="post" className="form">
+                <div className="form__field">
+                  <input type="text" placeholder="Name" />
+                </div>
+                <div className="form__field">
+                  <input type="email" placeholder="Email" />
+                </div>
+
+                <div className="form__field">
+                  <input type="password" placeholder="Password" />
+                </div>
+
+                <div className="form__field">
+                  <input type="submit" value="Sign Up" />
+                </div>
+              </form>
+
+              <p>
+                Already have an accout? <a href="/">Log in</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div>
       <Menu />
-      <div className="signup"></div>
+      {signupForm()}
     </div>
   );
 };
