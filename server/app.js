28313f2c437fe90f9f325a11c98c792d4e4105d0
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-02-06 14:06:10 +0530

    Dockerized the whole app.

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === "production") {
   //set static folder
   app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
