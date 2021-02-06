619bff72f8f132f15037603ea93774bf47052cca
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-02-06 12:20:32 +0530

    Dockerfile and Makefile for server completed

--- server/app.js
+++ server/app.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 //PORT
-const port = process.env.PORT || 8001;
+const port = process.env.PORT || 8000;
 
 //Starting a server
 app.listen(port, () => {
