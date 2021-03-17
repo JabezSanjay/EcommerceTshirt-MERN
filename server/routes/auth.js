31c72af04b1af0da858d03100144b120da40bde2
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-03-17 22:20:39 +0530

    Minor fixes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const { check, validationResult } = require("express-validator");
+const { check } = require("express-validator");
 const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
 
 router.post(
@@ -8,7 +8,9 @@ router.post(
   [
     check("name", "name should be at least 3 char").isLength({ min: 3 }),
     check("email", "email is required").isEmail(),
-    check("password", "password should be at least 3 char").isLength({ min: 3 })
+    check("password", "password should be at least 3 char").isLength({
+      min: 3,
+    }),
   ],
   signup
 );
@@ -17,7 +19,7 @@ router.post(
   "/signin",
   [
     check("email", "email is required").isEmail(),
-    check("password", "password field is required").isLength({ min: 1 })
+    check("password", "password field is required").isLength({ min: 1 }),
   ],
   signin
 );
